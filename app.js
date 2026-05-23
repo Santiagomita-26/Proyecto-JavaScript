fa94a2843bc11ccd9d729a2c440fd71997360759
santiago <santimendoza0207@gmail.com>
2026-05-22 19:03:52 -0500

    Feat ✨ : Async await ( api openweather )

--- app.js
+++ app.js
@@ -40,3 +40,51 @@ const editarGradoEstudiante=document.getElementById("editarGradoEstudiante")
 const editarTelefonoEstudiante=document.getElementById("editarTelefonoEstudiante")
 const guardarCambiosEstudiante=document.getElementById("guardarCambiosEstudiante")
 const cerrarModalEstudiante=document.getElementById("cerrarModalEstudiante")
+
+
+
+
+//Api del clima
+
+const temperatura = document.getElementById("temperatura");
+const descripcionClima = document.getElementById("descripcionClima");
+const iconoClima = document.getElementById("iconoClima");
+
+    async function obtenerClima(){
+
+    try{
+
+        const apiKey = "8f2967ecb2ce500349b36fa0b1e78af7";
+
+        const respuesta = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=Bucaramanga&appid=${apiKey}&units=metric&lang=es`
+        );
+
+        const datos = await respuesta.json();
+
+        console.log(datos);
+
+        if(datos.cod !== 200){
+
+            temperatura.textContent = "No disponible";
+            descripcionClima.textContent = datos.message;
+
+            return;
+        }
+
+        temperatura.textContent = Math.round(datos.main.temp) + "°C";
+        descripcionClima.textContent = datos.weather[0].description;
+        const icono = datos.weather[0].icon;
+        iconoClima.src =`https://openweathermap.org/img/wn/${icono}@2x.png`;
+
+     
+
+    }catch(error){
+
+        temperatura.textContent = "Error";
+
+        console.log(error);
+    }
+}
+
+obtenerClima();
