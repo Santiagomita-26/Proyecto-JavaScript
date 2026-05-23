8dc885912a4663ab8f1333df9fde7ce71770b1a2
santiago <santimendoza0207@gmail.com>
2026-05-22 19:00:47 -0500

    Feat ✨ : Carpeta js e inicio del DOM

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+//Formulario de RUTAS
+
+const nombreRuta = document.getElementById("nombreRuta");
+const nombreConductor = document.getElementById("conductor");
+const hora = document.getElementById("horaSalida");
+const ciudad = document.getElementById("ciudad");
+const crearRuta = document.getElementById("btnCrearRuta");
+const contenedorRutas= document.getElementById("contenedorRutas");
+
+//Formulario de ESTUDIANTES
+
+const nombreEstudiante = document.getElementById("nombreEstudiante");
+const grado = document.getElementById("grado");
+const telefono=document.getElementById("telefono");
+const selectRuta = document.getElementById("selectRuta");
+const btnAgregarEstudiante = document.getElementById("btnAgregarEstudiante");
+
+
+//Formulario EDITAR RUTAS
+
+const modalEditarRuta=document.getElementById("modalEditarRuta");
+const editarNombreRuta=document.getElementById("editarNombreRuta");
+const editarConductorRuta=document.getElementById("editarConductorRuta");
+const editarHoraRuta=document.getElementById("editarHoraRuta");
+const editarCiudadRuta=document.getElementById("editarCiudadRuta");
+const guardarCambiosRuta=document.getElementById("guardarCambiosRuta");
+const cerrarModalRuta=document.getElementById("cerrarModalRuta");
+
+
+//Formulario EDITAR ESTUDIANTES
+
+const modalEditarEstudiante=document.getElementById("modalEditarEstudiante")
+const editarNombreEstudiante=document.getElementById("editarNombreEstudiante")
+const editarGradoEstudiante=document.getElementById("editarGradoEstudiante")
+const editarTelefonoEstudiante=document.getElementById("editarTelefonoEstudiante")
+const guardarCambiosEstudiante=document.getElementById("guardarCambiosEstudiante")
+const cerrarModalEstudiante=document.getElementById("cerrarModalEstudiante")
